45e50d0b51413c2d7b5fb41eab7242de601fc52d
D4rthNihilus <rafaelguedes05013@gmail.com>
2025-08-04 09:09:30 -0300

    js adjustments

--- Projetos/tonpedia/scripts/script.js
+++ Projetos/tonpedia/scripts/script.js
@@ -35,10 +35,10 @@ const menuIcon = document.querySelector('#menuIcon svg');
 const lang = document.querySelector('#languages');
 
 document.querySelector('#translateIcon').addEventListener('click', ()=>{
-    if(!lang.style.display=='flex'){
-        lang.style.display='none'
+    if(lang.style.display!='flex'){
+        lang.style.display='flex'
     } else {
-        lang.style.display='flex';
+        lang.style.display='none';
     }
 });
 
@@ -67,6 +67,7 @@ moon.addEventListener('click', ()=>{
     if(document.body.style.backgroundColor=='black'){
         document.body.style.backgroundColor='white';
         document.body.style.color='black';
+        moon.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>';
         document.querySelector('table').style.backgroundColor = 'white';
         let links = [...document.querySelectorAll('a')];
         links.map((el)=>{
@@ -154,6 +155,7 @@ moon.addEventListener('click', ()=>{
         document.body.style.backgroundColor='black';
         document.body.style.color='white';
         document.querySelector('table').style.backgroundColor = 'black';
+        moon.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343"><path d="M440-760v-160h80v160h-80Zm266 110-55-55 112-115 56 57-113 113Zm54 210v-80h160v80H760ZM440-40v-160h80v160h-80ZM254-652 140-763l57-56 113 113-56 54Zm508 512L651-255l54-54 114 110-57 59ZM40-440v-80h160v80H40Zm157 300-56-57 112-112 29 27 29 28-114 114Zm283-100q-100 0-170-70t-70-170q0-100 70-170t170-70q100 0 170 70t70 170q0 100-70 170t-170 70Zm0-80q66 0 113-47t47-113q0-66-47-113t-113-47q-66 0-113 47t-47 113q0 66 47 113t113 47Zm0-160Z"/></svg>';
         let links = [...document.querySelectorAll('a')];
         links.map((el)=>{
             el.style.color='#4ec94e';
