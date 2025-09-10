d9974fb501c7d982a3b0edc9c9fda42144fcffc8
D4rthNihilus <rafaelguedes05013@gmail.com>
2025-09-09 21:37:34 -0300

    almost done

---
+++ Projetos/chords/script.js
@@ -0,0 +1,404 @@
+let word = document.querySelector('div#word input');
+let create = document.querySelector('input#button');
+let counter = 0;
+let vector = [];
+
+document.getElementById('l1').addEventListener('click', (e)=>{
+    counter++;
+    word.value+=e.target.innerHTML;
+    vector.push(1);
+    
+});
+
+document.getElementById('l2').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l3').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l4').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l5').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    vector.push(5);
+});
+
+document.getElementById('l6').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l7').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l8').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l9').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l10').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l11').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l12').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l13').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l14').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l15').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l16').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l17').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l18').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l19').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l20').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l21').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l22').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l23').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l24').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l25').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l26').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l27').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l28').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l29').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l30').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l31').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l32').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l33').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l34').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l35').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l36').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l37').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l38').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l39').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l40').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l41').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l42').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l43').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l44').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l45').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l46').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l47').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l48').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l49').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l50').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l51').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l52').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l53').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l54').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l55').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l56').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l57').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(1);
+});
+
+document.getElementById('l58').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(2);
+});
+
+document.getElementById('l59').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(3);
+});
+
+document.getElementById('l60').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(4);
+});
+
+document.getElementById('l61').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(5);
+});
+
+document.getElementById('l62').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(6);
+});
+
+document.getElementById('l63').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push(7);
+});
+
+document.getElementById('l64').addEventListener('click', (e)=>{
+    word.value+=e.target.innerHTML;
+    counter++;
+    vector.push('♮');
+});
+
+create.addEventListener('click', ()=>{
+    if (create.value!='CLEAN') {
+        if (word.value==''){
+            alert('Digite uma palavra!')
+        } else {
+            create.value = 'CLEAN';
+            word.value=(vector.join('-'));
+            counter = 0;
+        }  
+    } else {
+        word.value='';
+        create.value = 'CREATE';
+        vector = [];
+    }
+})
